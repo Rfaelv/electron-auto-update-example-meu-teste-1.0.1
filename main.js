a4b95f418f3e57db57bd3e894d30a6553005ed60
Rafael.V <ravieira@id.uff.br>
2021-05-03 18:25:03 -0300

    v9

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: "assets/icon.ico",
     webPreferences: {
       nodeIntegration: true,
     },
